0e8ccacbc8c7925c8661ef7a2aea4e1a7652ce83
shahreen-simin <shahreen.rahman18@northsouth.edu>
2023-06-14 21:33:38 +0600

    done

--- index.js
+++ index.js
@@ -10,18 +10,33 @@ app.use(express.json());
 
 const uri = "mongodb+srv://shahreenrahman18:shahreen1234@cluster0.yube51c.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
 
 async function run(){
     try {
         await client.connect();
+        const registerCollection = client.db('Education_Management_System').collection('register');
         const postCollection = client.db('Education_Management_System').collection('posts');
         const jobPostCollection = client.db('Education_Management_System').collection('jobs');
+        const courseCollection = client.db('Education_Management_System').collection('courses');
+
+        app.get('/register', async(req, res)=>{
+            // const query = {};
+            const cursor =  registerCollection.find({});
+             const register = await cursor.toArray();
+            res.send(register);
+        })
+        
+        app.post('/register', async(req, res)=>{
+            const register = req.body;
+            const result = await registerCollection.insertOne(register);
+            return res.send(result);
+        })
 
         app.get('/posts', async(req, res)=>{
             // const query = {};
@@ -47,6 +62,20 @@ async function run(){
         app.post('/jobs', async(req, res)=>{
             const jobs = req.body;
             const result = await jobPostCollection.insertOne(jobs);
+            return res.send(result);
+        })
+
+        
+        app.get('/courses', async(req, res)=>{
+            // const query = {};
+            const cursor =  courseCollection.find({});
+            const course = await cursor.toArray();
+            res.send(course);
+        })
+        
+        app.post('/courses', async(req, res)=>{
+            const course = req.body;
+            const result = await courseCollection.insertOne(course);
             return res.send(result);
         })
     } 
